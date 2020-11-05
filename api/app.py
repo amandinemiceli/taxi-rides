abd6114036150d7b0ced4810f089800cc4b29a93
Amandine Miceli <amandine.miceli@job2day.fr>
2020-11-06 00:07:54 +0100

    Added rides + renamed returned code var from api

--- api/app.py
+++ api/app.py
@@ -27,6 +27,36 @@ RIDES = [
         "distance": 5,
         "startTime": "2020-06-19T14:01:17.031Z",
         "duration": 7000
+    },
+    {
+        "id": 4,
+        "distance": 4,
+        "startTime": "2020-06-19T08:32:45.031Z",
+        "duration": 6500
+    },
+    {
+        "id": 5,
+        "distance": 2,
+        "startTime": "2020-10-24T23:06:27.031Z",
+        "duration": 3000
+    },
+        {
+        "id": 6,
+        "distance": 1,
+        "startTime": "2020-10-29T18:35:22.391Z",
+        "duration": 9000
+    },
+    {
+        "id": 7,
+        "distance": 3,
+        "startTime": "2020-11-01T11:28:05.031Z",
+        "duration": 5000
+    },
+    {
+        "id": 8,
+        "distance": 1,
+        "startTime": "2020-11-06T03:55:07.031Z",
+        "duration": 7000
     }
 ]
 
@@ -36,7 +66,7 @@ def get_all_rides():
     if RIDES is None:
         return jsonify(
             {
-                'code': 404,
+                'status_code': 404,
                 'success': False,
                 'message': 'Error. Failed to retrieve rides.'
             }
@@ -44,7 +74,7 @@ def get_all_rides():
     else:
         return jsonify(
             {
-                'code': 200,
+                'status_code': 200,
                 'status': True,
                 'data': RIDES
             }
