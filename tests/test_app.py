91dfcca6ec249b350507db24e54e54ff2de9d6b6
Amandine Miceli <amandine.miceli@job2day.fr>
2020-11-07 17:39:55 +0100

    Added tests directory

---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+import json
+
+from tests.config import app, client
+
+
+def test_index(client):
+    """initial test. ensure that the root route is available."""
+    response = client.get("/")
+    assert response.status_code == 200
+    expected = {'hello': 'world'}
+    assert expected == json.loads(response.get_data(as_text=True))
