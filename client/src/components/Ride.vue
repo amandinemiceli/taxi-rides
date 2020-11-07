e37dbdb16df95101e061f85be6741888226c49c7
Amandine Miceli <amandine.miceli@job2day.fr>
2020-11-07 18:57:26 +0100

    Reformatted code to add Ride component

---
+++ client/src/components/Ride.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div v-bind:id="ride.id">
+            <p>Ride: {{ ride.id }}</p>
+            <p>Price: {{ ride.price }} EUR</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Ride',
+    props: ['ride']
+};
+</script>
--- client/src/components/RideList.vue
+++ client/src/components/RideList.vue
@@ -3,28 +3,25 @@
         <h3 class="text-lg">{{ title }}</h3>
         <div class="grid grid-cols-3 xl:grid-cols-5 place-content-center mt-8"
           v-if="content.length > 0">
-            <div class="box-content border-2 m-3 p-3"
+            <Ride class="box-content border-2 m-3 p-3"
+              v-bind:class="{ 'bg-red-500': ride.distance > 2 }"
               v-for="(ride, index) in content"
               v-bind:key="index"
-              v-bind:id="ride.id"
-              v-bind:class="{ 'bg-red-500': ride.distance > 2 }"
-            >
-                <p>Ride ID: {{ ride.id }}</p>
-                <p>Distance: {{ ride.distance }}</p>
-                <p>Start time: {{ ride.startTime }}</p>
-                <p>Duration: {{ ride.duration }}</p>
-            </div>
+              v-bind:ride="ride"
+            />
         </div>
         <div class="error-message" v-else>{{ error }}</div>
     </div>
 </template>
 
 <script>
+import Ride from './Ride';
 import axios from 'axios';
 
 export default {
     name: 'RideList',
-    components: {},
+    components: { Ride },
+    props: ['ride'],
     data() {
         return {
             title: 'Here are the different rides available!',
