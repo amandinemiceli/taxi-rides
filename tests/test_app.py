b3e342300240525fc4a39fe03a8a86c035c33ed8
Amandine Miceli <amandine.miceli@job2day.fr>
2020-11-09 23:42:50 +0100

    Added tests to validate endpoints

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,7 @@
+import pytest
 import json
+import api.settings as app_settings
+import api.app as api
 
 from tests.conftest import app, client
 
@@ -9,3 +12,29 @@ def test_index(client):
     assert response.status_code == 200
     expected = {'hello': 'world'}
     assert expected == json.loads(response.get_data(as_text=True))
+
+
+def test_get_all_rides(client):
+    response = client.get("/rides")
+    assert response.status_code == 200
+    response_data = json.loads(response.get_data(as_text=True))
+    assert 'data' in response_data
+    assert isinstance(response_data['data'], list)
+
+
+def test_get_ride(client):
+    rideId: int = 6
+    response = client.get("/rides/" + str(rideId))
+    assert response.status_code == 200
+    response_data = json.loads(response.get_data(as_text=True))
+    assert 'data' in response_data
+    assert isinstance(response_data['data'], dict)
+
+
+def test_get_ride_none(client):
+    rideId: int = 0
+    response = client.get("/rides/" + str(rideId))
+    assert response.status_code == 404
+    response_data = json.loads(response.get_data(as_text=True))
+    assert 'message' in response_data
+    assert isinstance(response_data['message'], str)
